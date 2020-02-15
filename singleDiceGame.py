c8524775fc73ec7912862c745069e4092bbb763c
Michael <Michael.Tighe92@gmail.com>
2020-02-15 17:51:10 -0500

    fixed small error regarding count

--- singleDiceGame.py
+++ singleDiceGame.py
@@ -4,23 +4,23 @@ import random
 
 random.seed()
 
-# functions
+# function
 # calculateGame determines if player or CPU wins or if draw
 
-def calculateGame (player, CPU, cnt):
+def calculateGame (player, CPU, count):
     if player > CPU:
-        print(f"Player Wins round {cnt}!")
+        print(f"Player Wins round {count}!")
         return True
     elif CPU > player:
-        print(f"CPU Wins round {cnt}!")
+        print(f"CPU Wins round {count}!")
         return False
     else:
-        print(f"Round {cnt} ends in a draw!")
+        print(f"Round {count} ends in a draw!")
 
 # variables
 playerWins = 0
 CPUWins = 0
-cnt = 1
+count = 0
 whoWon = 0
 
 print("----- Welcome to single dice game! ------")
@@ -29,17 +29,17 @@ print("1.) You or CPU must reach 5 wins in order to end the game")
 print("2.) Draws do not count as either a win or loss")
 
 while playerWins < 5 and CPUWins < 5:
+    count += 1
     player = random.randint(1,6)
     CPU = random.randint(1,6)
     print(f"player's die = {player}, CPU's die = {CPU}")
-    whoWon = calculateGame(player,CPU,cnt)
+    whoWon = calculateGame(player,CPU,count)
     if whoWon == True:
         playerWins += 1
     elif whoWon == False:
         CPUWins += 1
-    cnt += 1
 
 if playerWins > CPUWins:
-    print(f"Player won and finished the game in round {cnt}!!!")
+    print(f"Player won and finished the game in round {count}!!!")
 else:
-    print(f"CPU won and finished the game in round {cnt}!!!")
+    print(f"CPU won and finished the game in round {count}!!!")
